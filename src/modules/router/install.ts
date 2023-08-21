3c3e8d11ccb6822e7b7739639e1bbe01811af91d
倚窗听雨丿醉无心 <994039348@qq.com>
2023-08-21 09:19:17 +0800

    切换路由将滚动条重置至顶部

--- src/modules/router/install.ts
+++ src/modules/router/install.ts
@@ -4,6 +4,7 @@ import routes from './routes';
 const router = createRouter({
   history: createWebHistory(),
   routes,
+  scrollBehavior: () => ({ left: 0, top: 0 }),
 });
 
 export default router;
